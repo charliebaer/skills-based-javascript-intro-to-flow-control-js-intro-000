8ec5251caee89752c1afe07d9f1b474b8c44e3e2
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-07 12:31:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,7 @@ function switchAge(age) {
     case(age>=13 && age<=19):
                   return('You are a teenager');
                   break;
-  default:
+    case(age):
       return('You have an age');
 }
 }
