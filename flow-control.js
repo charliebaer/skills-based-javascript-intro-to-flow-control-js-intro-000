dedb80dd680ee2400190ebe7af685ae4bbae7059
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-07 12:16:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,7 +20,7 @@ else return('You are a grownup')
   }
 
 function ternaryTeenager(age) {
-(age>=13 && age<=19) ? ('You are a teenager!') : ('You are not a teenager')
+(age>=13 && age<=19) ? ('You are a teenager') : ('You are not a teenager')
 }
 
 function switchAge(age) {
