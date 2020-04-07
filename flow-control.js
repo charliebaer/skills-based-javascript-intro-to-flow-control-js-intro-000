5f4feee67e595df9574b250f2d9260afbcc34260
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-07 12:33:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,12 +24,9 @@ return (age>=13 && age<=19) ? 'You are a teenager' : 'You are not a teenager'
 }
 
 function switchAge(age) {
-  switch(age){
-    case(age>=13 && age<=19):
-        return('You are a teenager');
-                  break;
-    case(age<=12 && age>=20):
-            return('You have an age');
-      break;
+  if(age>=13 && age<=19)
+    return('You are a teenager!');
+  else
+    return('You are not a teenager');;
 }
 }
